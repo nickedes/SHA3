063b21521dc8923c69c0793febb7c848d296b3c6
nikhil <nkmittal4994@gmail.com>
2018-03-02 21:51:53 +0530

    step mapping implemented

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from math import log2
+
+
 def getState(S, w):
     """
     S is the string of b bits.
@@ -53,6 +56,100 @@ def theta(A, w):
                 A_[x][y][z] = A[x][y][z] ^ D[x][z]
 
     return A_
+
+
+def rho(A, w):
+    """
+    rho step mapping - Rotate the bits of each lane by an offset.
+    """
+    # init for A_ ?
+    A_ = [[[0 for z in range(w)] for y in range(5)] for x in range(5)]
+    for z in range(w):
+        A_[0][0][z] = A[0][0][z]
+    x, y = 1, 0
+    for t in range(24):
+        for z in range(w):
+            A_[x][y][z] = A[x][y][(z - (t+1)*(t+2)//2) % w]
+        x, y = y, (2*x + 3*y) % w
+    return A_
+
+
+def pi(A, w):
+    """
+    pi step mapping - Rearrange the positions of the lanes
+    """
+    A_ = [[[0 for z in range(w)] for y in range(5)] for x in range(5)]
+
+    for x in range(5):
+        for y in range(5):
+            for z in range(w):
+                A_[x][y][z] = A[(x + 3*y) % 5][x][z]
+    return A_
+
+
+def chi(A):
+    """
+    Chi step mapping - XOR each bit with Non linear operation of two other bits in the same row
+    """
+    A_ = [[[0 for z in range(w)] for y in range(5)] for x in range(5)]
+    for x in range(5):
+        for y in range(5):
+            for z in range(w):
+                A_[x][y][z] = A[x][y][z] ^ (
+                    (A[(x+1) % 5][y][z] ^ 1) & (A[(x+2) % 5][y][z]))
+    return A_
+
+
+def rc(t):
+    """
+    Round constant for iota step mapping
+    """
+    if t % 255 == 0:
+        return 1
+    R = [1, 0, 0, 0, 0, 0, 0, 0]
+    for i in range(1, t % 255):
+        R = [0] + R
+        R[0] = R[0] ^ R[8]
+        R[4] = R[4] ^ R[8]
+        R[5] = R[5] ^ R[8]
+        R[6] = R[6] ^ R[8]
+        R = R[:8]
+    return R[0]
+
+
+def iota(A, i, w):
+    """
+    Iota step mapping - modify some bits of (0, 0) Lane based on round constant
+    """
+    A_ = [[[A[x][y][z] for z in range(w)] for y in range(5)] for x in range(5)]
+    RC = [0]*w
+    l = int(log2(w))
+    for j in range(l+1):
+        RC[2**j - 1] = rc(j + 7*i)
+
+    for z in range(w):
+        A_[0][0][z] = A_[0][0][z] ^ RC[z]
+    return A_
+
+
+def round(A, i):
+    """
+    A - state
+    i - round index
+    """
+    return iota(chi(pi(theta(A, w), w), w), i, w)
+
+
+def keccak(S, nr):
+    """
+    """
+    w = len(S)//25
+    l = int(log2(w))
+    A = getState(S, w)
+    for i in range(12+2*l - nr, 12 + 2*l):
+        A = round(A, i)
+    S = getString(A, w)
+    return S
 
 
 w = 64
