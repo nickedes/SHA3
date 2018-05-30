20efba8faa3e9b2163aca8b8200bfe19a09b773c
nikhil <nkmittal4994@gmail.com>
2018-05-30 12:22:18 +0530

    get change in bits

--- kernel.py
+++ kernel.py
@@ -49,10 +49,13 @@ def statePrint(A, w, statebitlist):
             for x in range(5):
                 if A[x][y][z] == 1:
                     flag = 1
+                    if (x, y, z) not in statebitlist:
+                        alpha = (x, y, z)
                 s += str(A[x][y][z]) + " "
             s += "\n"
         if flag:
             print(s)
+    return alpha
 
 
 def main():
@@ -62,31 +65,38 @@ def main():
     start = []
     for x in range(5):
         for y in range(5):
-            start.append((x,y))
+            start.append((x, y))
 
-    _x, _y = 3,1
+    _x, _y, _z = 3, 1, 0
 
-    orig_state = [(_x, _y)]
-
+    orig_state = []
     next_state = []
 
-    A[_x][_y][0] = 1
-    statePrint(A, w)
+    A[_x][_y][_z] = 1
+    orig_state.append(statePrint(A, w, orig_state))
     for i in range(5):
         if i % 2 == 0:
             A = rho(A, w)
             A = pi(A, w)
-            next_state.append((_x, _y))
+            new_pos = statePrint(A, w, next_state)
+            if new_pos == '':
+                print("done")
+            else:
+                next_state.append(new_pos)
+            print(next_state)
         else:
             A = piInverse(A, w)
             A = rhoInverse(A, w)
-            orig_state.append((_x, _y))
-        statePrint(A, w)
+            new_pos = statePrint(A, w, orig_state)
+            if new_pos == '':
+                print("done")
+            else:
+                orig_state.append(new_pos)
+            print(orig_state)
         print("Enter x, y, z :")
         line = input()
         _x, _y, _z = [int(j) for j in line.split(" ")]
         A[_x][_y][_z] = 1
-
 
 
 if __name__ == '__main__':
