9431fc016c3f78652decfb49c2bcf7291c3b1eeb
nikhil <nkmittal4994@gmail.com>
2018-05-31 22:41:40 +0530

    specify not allowed bits in delta1 and delta3

--- kernel.py
+++ kernel.py
@@ -92,6 +92,14 @@ def main():
     w = 64
     A = [[[0 for z in range(w)] for y in range(5)] for x in range(5)]
     start = []
+
+    # differential path requirements
+    # input diff cant have these
+    negative_init = [(2, 3), (2, 4), (3, 0), (3, 1), (3, 2),
+                     (3, 3), (3, 4), (4, 0), (4, 1), (4, 2), (4, 3), (4, 4)]
+
+    negative_out = [(0, 0), (0, 1), (0, 2), (0, 3), (0,4), (1, 0)]
+
     for x in range(5):
         for y in range(5):
             start.append((x, y, 0))
