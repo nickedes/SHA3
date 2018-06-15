0a137f06f7f08ac94ce1ad59bd3e0cfc93d8afa7
nikhil <nkmittal4994@gmail.com>
2018-06-15 12:35:21 +0530

    start and end states negative list

--- kernel.py
+++ kernel.py
@@ -105,11 +105,11 @@ def main():
     start = []
 
     # differential path requirements
-    # input diff cant have these
+    # input diff cant have these lanes
     negative_init = [(2, 3), (2, 4), (3, 0), (3, 1), (3, 2),
                      (3, 3), (3, 4), (4, 0), (4, 1), (4, 2), (4, 3), (4, 4)]
 
-    negative_out = [(0, 0), (0, 1), (0, 2), (0, 3), (0,4), (1, 0)]
+    negative_out = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 0)]
 
     for x in range(5):
         for y in range(5):
