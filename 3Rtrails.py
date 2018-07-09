4886e8da2056f807eaf4f81b24c601f71be0109a
nickedes <nkmittal4994@gmail.com>
2018-07-09 18:03:55 +0530

    removing cases by adding more conditions

--- 3Rtrails.py
+++ 3Rtrails.py
@@ -25,42 +25,44 @@ def active6(w):
     trails = []
     for (x0, y0) in points:
         for (x1, y1) in points:
+            if x0 != x1 :
+                continue
             for (x2, y2) in points:
                 for (x3, y3) in points:
+                    if x2 != x3 :
+                        continue
                     for (x4, y4) in points:
                         for (x5, y5) in points:
-                            # for (x6, y6) in points:
-                            #     for (x7, y7) in points:
-                                    # from I, II and III
-                                    if x0 == x1 and x2 == x3 and x4 == x5\
-                                     and y1 == y2 and y3 == y4 and y5 == y0:
-                                        # z0 == z1 and z2 == z3 and z4 == z5
-                                        # from IV
-                                        # dv = rho(x0, y0, w) + rho(x2, y2, w) + rho(x4, y4, w) + rho(x6, y6, w) - (rho(x1, y1, w) + rho(x3, y3, w) + rho(x5, y5, w) + rho(x7, y7, w))
-                                        dv = rho(x0, y0, w) + rho(x2, y2, w) + rho(x4, y4, w) - (rho(x1, y1, w) + rho(x3, y3, w) + rho(x5, y5, w))
-                                        dv = abs(dv)
-                                        if dv % w == 0:
-                                            if pi(x1 , y1)[0] == pi(x2, y2)[0] and pi(x3 , y3)[0] == pi(x4, y4)[0] and pi(x5, y5)[0] == pi(x0, y0)[0]:
-                                                # choose z0 freely
-                                                z0 = r(0, 5)
-                                                z2 = (z0 + rho(x1, y1, w) - rho(x2, y2, w)) % w
-                                                z4 = (z2 + rho(x3, y3, w) - rho(x4, y4, w)) % w
-                                                # verification step
-                                                if z0 != ( (z4 + rho(x5, y5, w) - rho(x0, y0, w)) % w ):
-                                                    continue
-                                                if dv > 0:
-                                                    Cv = max(Cv, log2(dv))
-                                                a00 = (x0, y0, z0)
-                                                a01 = (x1, y1, z0)
-                                                a02 = (x2, y2, z2)
-                                                a03 = (x3, y3, z2)
-                                                a04 = (x4, y4, z4)
-                                                a05 = (x5, y5, z4)
-                                                if not allunique(a00, a01, a02, a03, a04, a05):
-                                                    continue
-                                                c += 1
-                                                # print(a00, a01, a02, a03, a04, a05)
-                                                trails.append( [a00, a01, a02, a03, a04, a05] )
+                            # from I, II and III
+                            if x0 == x1 and x2 == x3 and x4 == x5\
+                             and y1 == y2 and y3 == y4 and y5 == y0:
+                                # z0 == z1 and z2 == z3 and z4 == z5
+                                # from IV
+                                # dv = rho(x0, y0, w) + rho(x2, y2, w) + rho(x4, y4, w) + rho(x6, y6, w) - (rho(x1, y1, w) + rho(x3, y3, w) + rho(x5, y5, w) + rho(x7, y7, w))
+                                dv = rho(x0, y0, w) + rho(x2, y2, w) + rho(x4, y4, w) - (rho(x1, y1, w) + rho(x3, y3, w) + rho(x5, y5, w))
+                                dv = abs(dv)
+                                if dv % w == 0:
+                                    if pi(x1 , y1)[0] == pi(x2, y2)[0] and pi(x3 , y3)[0] == pi(x4, y4)[0] and pi(x5, y5)[0] == pi(x0, y0)[0]:
+                                        # choose z0 freely
+                                        z0 = r(0, 5)
+                                        z2 = (z0 + rho(x1, y1, w) - rho(x2, y2, w)) % w
+                                        z4 = (z2 + rho(x3, y3, w) - rho(x4, y4, w)) % w
+                                        # verification step
+                                        if z0 != ( (z4 + rho(x5, y5, w) - rho(x0, y0, w)) % w ):
+                                            continue
+                                        if dv > 0:
+                                            Cv = max(Cv, log2(dv))
+                                        a00 = (x0, y0, z0)
+                                        a01 = (x1, y1, z0)
+                                        a02 = (x2, y2, z2)
+                                        a03 = (x3, y3, z2)
+                                        a04 = (x4, y4, z4)
+                                        a05 = (x5, y5, z4)
+                                        if not allunique(a00, a01, a02, a03, a04, a05):
+                                            continue
+                                        c += 1
+                                        # print(a00, a01, a02, a03, a04, a05)
+                                        trails.append( [a00, a01, a02, a03, a04, a05] )
     print("No. of trails = ", c, ", Character of Vortex : ", Cv)
     return trails
 
