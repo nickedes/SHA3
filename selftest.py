ed07b134b106145bd5ee49604bf267f4faeeba09
nickedes <nkmittal4994@gmail.com>
2018-07-11 20:25:12 +0530

    self test file

--- selftest.py
+++ selftest.py
@@ -1,5 +1,23 @@
+from chi_diff import applyrho
 from kernel import getBitposAfterOneRhoPi as lol
+from kernel import getOneBitPos
+import main
 
-line = input().split(" ")
+# line = input().split(" ")
+# w = 64
+# print(lol(int(line[0]), int(line[1]), w))
+
 w = 64
-print(lol(int(line[0]), int(line[1]), w))
+for x in range(5):
+	for y in range(5):
+		for z in range(w):
+			o1 = applyrho(x,y,z)
+
+			A = [[[0 for k in range(w)] for j in range(5)] for i in range(5)]
+			A[x][y][z] = 1
+
+			A = main.rho(A, w)
+			o2 = getOneBitPos(A, w)[0]
+			if o1 != o2:
+				print(x,y,z)
+				input()
