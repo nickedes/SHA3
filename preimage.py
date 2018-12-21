831c6382a97a1a02771e6e7b8c9752a9aafa2cd2
nickedes <nkmittal4994@gmail.com>
2018-12-21 17:12:38 +0530

    rewriting attack on Keccak[r:=400-192, c:=192]

---
+++ preimage.py
@@ -0,0 +1,29 @@
+from main import *
+import itertools
+import multiprocessing as mp
+
+
+def getreversePrintformat(digest):
+    """
+        Input is hex digest
+        For example : 1a cc 22 0b 9e 2e a7 20 8d 48 61 ba b4 30 ae 45 b0 fc 48 10 62 1f 19 20 (or) 1acc220b9e2ea7208d4861bab430ae45b0fc4810621f1920
+        Return Binary digest
+    """
+    out = ""
+    for i in range(len(digest)//2):
+        out += "{:08b}".format( int( digest[i*2 : (i+1)*2], 16) )[::-1]
+    return out
+
+
+if __name__ == '__main__':
+
+	state_size = 400
+
+	# lane size
+	w = state_size//25
+
+	hexdigest = "100000000000000000000000"
+
+	bitdigest = getreversePrintformat(hexdigest)
+
+
